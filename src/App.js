6dfaba81ac3d3de7db9a8cb8b9f196e691e6ef81
shibin <shibinashraf36@gmail.com>
2023-07-25 12:07:43 +0530

    solved responsive

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 const App = () => {
   return (
     <>
-      <div className="Main">
-        <ul className="flex justify-evenly mt-4 text-red-600 font-bold">
+      <div className="">
+        <ul className="flex justify-evenly pt-4 text-red-600 font-bold bg-slate-800 ">
           <li className="">
             <Link to="QrCode">QrCode</Link>
           </li>
@@ -17,10 +17,15 @@ const App = () => {
             <Link to="QrCode">QrCode</Link>
           </li>
         </ul>
+        <div className="bg-slate-800  h-screen flex justify-center items-center">
+          <h1 className=" m-auto text-slate-100 text-xl">
+            View my frontend practice projects by clicking on navbar items
+          </h1>
+        </div>
+        <Routes>
+          <Route path="QrCode" element={<QrCode />} exact />
+        </Routes>
       </div>
-      <Routes>
-        <Route path="QrCode" element={<QrCode />} />
-      </Routes>
     </>
   );
 };
--- src/QrCode.jsx
+++ src/QrCode.jsx
@@ -4,13 +4,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const QrCode = () =>{
     return(
-    <body className="h-screen bg-slate-400 items-center justify-center flex">
-        <div className="h-3/5 w-1/6 bg-white  rounded-xl flex-column p-4">
+    <div className="h-screen bg-teal-100 items-center justify-center flex">
+        <div className="w-[50%] md:w-[20%] lg:w-[20%] shadow-lg bg-white  rounded-xl flex-column p-4">
             <img className='w-full h-2/3 rounded-xl' src={ qr } alt="qr"></img>
             <p className="text-center font-bold	">Improve Your Frontend skills by doing projects</p>
             <p className="text-center font-thin">Scan the qr to visit frontend mentor and take your coding skills to the next level.</p>
         </div>
-    </body>
+    </div>
     )
 }
 export default QrCode;
